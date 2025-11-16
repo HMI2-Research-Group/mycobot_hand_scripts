29b54a7906ba48f353a8dfcce34955c1b1d91a1b
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2025-11-15 23:30:37 -0800

    add some sleeps

--- launch_real_cobot.sh
+++ launch_real_cobot.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 screen -S base_teleop -dm roslaunch limo_base limo_base.launch
+sleep 5
 screen -S camera -dm roslaunch astra_camera dabai_u3.launch
+sleep 3
 screen -S lidar -dm roslaunch limo_bringup limo_start.launch pub_odom_tf:=false 
+sleep 3
 screen -S cobot_arm_moveit -dm roslaunch limo_cobot_moveit_config demo.launch
+sleep 5
 screen -S cobot_arm_uart -dm rosrun mycobot_280_moveit sync_plan.py _port:=/dev/ttyACM0 _baud:=115200
