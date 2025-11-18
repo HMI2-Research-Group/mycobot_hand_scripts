9fdc1d87b7565e7daeb076192b3533cdf8324418
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2025-11-18 05:54:40 -0800

    sync

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 cat << 'EOF'
+ ___| |_ __ _ _ __  __      ____ _ _ __ ___ 
+ / __| __/ _` | '__| \ \ /\ / / _` | '__/ __|
+ \__ \ || (_| | |     \ V  V / (_| | |  \__ \
+ |___/\__\__,_|_|      \_/\_/ \__,_|_|  |___/
+George Lucas - Jedi - Death Star - R2-D2 - C-3PO - Yoda - Darth Vader
+
+
+
   .   *   ..  . *  *
 *  * @()Ooc()*   o  .
     (Q@*0CG*O()  ___
@@ -13,5 +21,5 @@ cat << 'EOF'
    |\_|__|__|_/|
     \_________/
 
-So you want to run run the bot?? May I ask why?
+So you want to run run the bot?? Darth Vader is asking why? Is it to overthrow the empire? Is it to join the dark side? Answer wisely...
 EOF
