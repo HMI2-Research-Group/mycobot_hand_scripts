fdd47662bdb816e812988747ba6b16dfe4d41d8d
krishnachaitanya7 <chaitanyaradon89@gmail.com>
2025-12-04 23:52:02 -0800

    use numpy array, both should work

--- scripts/get_joint_angles.py
+++ scripts/get_joint_angles.py
@@ -2,6 +2,7 @@
 import time
 from math import pi
 
+import numpy as np
 from pymycobot.mycobot import MyCobot
 
 PORT = "/dev/ttyACM0"
@@ -11,9 +12,9 @@ if __name__ == "__main__":
     mc = MyCobot(PORT, BAUD)
     time.sleep(2.0)  # small delay to make sure serial is ready
 
-    angles = mc.get_angles()   # returns list of 6 joint angles in degrees
+    angles = np.array(mc.get_angles())   # returns list of 6 joint angles in degrees
     print("Current Joint Angles (degrees): \n", angles)
-    angles_rad = [angle * pi / 180.0 for angle in angles]
+    angles_rad = angles * (pi / 180.0)  # convert to radians
     print("Current Joint Angles (radians): \n", angles_rad)
     mc.release_all_servos()  # release the servos after getting angles
     print("Use Radians for MoveIt! planning.")
